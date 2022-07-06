24dc64c7c70e6f7a79ddf4408170bc3608088901
sirgama <sirengogamaliel@gmail.com>
2022-07-06 14:56:02 +0300

    setup function for totals and rendering it on cart page

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
-from multiprocessing import context
 from django.shortcuts import render
-
+from .models import *
+from users.models import Customer
 # Create your views here.
 def store(request):
     
@@ -13,10 +13,17 @@ def store(request):
 
 
 def cart(request):
-    
-    
-    context = {
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
         
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {
+        'items':items,
+        'order':order,
     }
     return render(request, 'shop/cart.html', context)
 
