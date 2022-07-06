659ca2d6c4ac89fbf6e4bbb5d789ccc392060ae0
sirgama <sirengogamaliel@gmail.com>
2022-07-06 07:09:28 +0300

    create customer model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
