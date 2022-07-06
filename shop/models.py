73756870b288b4ccb38039ee495b6efd3069453f
sirgama <sirengogamaliel@gmail.com>
2022-07-06 08:03:32 +0300

    update order, shipping, product and order items models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,44 @@
+from enum import _auto_null
+from this import d
 from django.db import models
+from users.models import Customer
+
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price= models.FloatField()
+    digit = models.BooleanField(default=False, null=True, blank=False)
+    
+    
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return str(self.id)
+    
+    
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+class Shipping(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    county = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.address
+    
