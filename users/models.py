51657df3888239e568368752da5d2c2c32b630d4
sirgama <sirengogamaliel@gmail.com>
2022-07-06 07:38:58 +0300

    update customer model with image

--- users/models.py
+++ users/models.py
@@ -1,12 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     
     def __str__(self):
         return self.name
+    @property
+    def username(self):
+        return self.user.username
+    
+    def __str__(self):
+        return f'{self.user.username} Customer'
+    
+    @receiver(post_save, sender=User)
+    def create_Customer(sender, instance, created, **kwargs):
+        if created:
+            Customer.objects.create(user=instance)
+            
+
+    @receiver(post_save, sender=User)
+    def save_Customer(sender, instance, **kwargs):
+        instance.Customer.save()
