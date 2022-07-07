3f18c11caf43bf0cb358505229454e10697f9a24
sirgama <sirengogamaliel@gmail.com>
2022-07-07 04:48:43 +0300

    add javascript function to get current user and button

---
+++ static/js/home.js
@@ -0,0 +1,12 @@
+console.log('hello world')
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (var i=0; i<updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log(productId, action)
+        console.log(user)
+
+    })
+}
