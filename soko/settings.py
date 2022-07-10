38e6a9ac73f2fe9e815d55e4720e628f06c55509
sirgama <sirengogamaliel@gmail.com>
2022-07-10 20:43:29 +0300

    install python daraja

--- soko/settings.py
+++ soko/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.github',
     'widget_tweaks',
+    'django_daraja',
     'shop',
     'users',
     'crispy_forms',
