3d03d06aed328a88817d6f1abe3df4b8427c7bb2
sirgama <sirengogamaliel@gmail.com>
2022-07-12 01:40:57 +0300

    setup view functin to filter men's outfits

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 import json
+from unicodedata import category
 from urllib import response
 from django.shortcuts import render
 from django.urls import reverse
@@ -130,9 +131,22 @@ def processOrder(request):
 @login_required
 def men(request):
     
-    
-    context = {
+    mens= Categories.objects.get(name='Men')
+    print(mens)
+    products = Product.objects.filter(category__name=mens)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartitems = order.get_cart_items
         
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        cartitems = order['get_cart_items']
+    context = {
+        'products':products,
+        'cartitems':cartitems
     }
     
     return render(request, 'shop/men.html', context)
